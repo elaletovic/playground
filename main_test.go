986d8098dd1886edd911910e47c580238ceeec00
Emin Laletovic <laletovic@gmail.com>
2020-12-16 23:18:04 +0100

    Added test for Preload with Select

--- main_test.go
+++ main_test.go
@@ -18,3 +18,47 @@ func TestGORM(t *testing.T) {
 		t.Errorf("Failed, got error: %v", err)
 	}
 }
+
+func TestPreloadWithSelect(t *testing.T) {
+	user1 := User{Name: "emin",
+		Languages: []Language{{
+			Name: "Bosnian",
+			Code: "ba",
+		},
+			{
+				Name: "English",
+				Code: "en",
+			},
+		},
+	}
+	user2 := User{Name: "jinzhu",
+		Languages: []Language{{
+			Name: "Chinese",
+			Code: "zh",
+		},
+			{
+				Name: "English",
+				Code: "en",
+			},
+		},
+	}
+
+	DB.Create(&user1)
+	DB.Create(&user2)
+
+	var users []User
+	//this works
+	if err := DB.Preload("Languages").Find(&users); err.Error != nil {
+		t.Errorf("failed, got error: %v", err.Error)
+	}
+	log.Println(users)
+
+	var users2 []User
+	//this works also
+	if err := DB.Preload("Languages", func(db *gorm.DB) *gorm.DB {
+		return db.Select("code", "name")
+	}).Find(&users2); err.Error != nil {
+		t.Errorf("failed, got error: %v", err.Error)
+	}
+	log.Println(users2)
+}
